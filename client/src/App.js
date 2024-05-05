318de368e8a130b70336b5e6093c1b84120491d8
MilanRB1 <milanrajbansi212@gmail.com>
2024-05-05 13:21:09 +0545

    login
    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes,Router, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Router, Route, Link } from "react-router-dom";
 // import "./App.scss";
 
 import Header from "./components/Header/Header";
@@ -14,29 +14,30 @@ import AboutPage from "./components/AboutPage/AboutPage";
 import { LoginRegister } from "./components/loginPage/LoginRegister";
 import Apps from "./components/Admin/strapi/strapi";
 import { useState } from "react";
+import Layout from "./components/Layout";
 function App() {
-    const [ShowFooter, setShowFooter] =useState(true)
-    return (
-        <BrowserRouter>
-            <AppContext>
-                <Header />
-                <Routes>
-                    <Route path="/"  element={<Home />}/>
-                    <Route path="/category/:id" element={<Category />} />
-                    <Route path="/product/:id" element={<SingleProduct />} />
-                    <Route path="/success" element={<SuccessPage />} />    
-                    <Route path="/about" element={<AboutPage />} />    
-                    <Route path="/login" element={<LoginRegister />} /> 
-                    <Route path="/admin" element={<Apps />} />
-                </Routes>
+  const [ShowFooter, setShowFooter] = useState(true);
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Header />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:id" element={<Category />} />
+            <Route path="/product/:id" element={<SingleProduct />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/admin" element={<Apps />} />
+          </Route>
+          <Route path="/login" element={<LoginRegister />} />
+        </Routes>
 
-                
-                <Newsletter/>
-                <Footer />
-                
-            </AppContext>
-        </BrowserRouter>
-    );
+        {/* <Newsletter/>
+                <Footer /> */}
+      </AppContext>
+    </BrowserRouter>
+  );
 }
 
 export default App;
