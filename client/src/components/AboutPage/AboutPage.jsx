4691cfd2ad0dbb44042dd402576ef3aeb9f1dede
MilanRB1 <milanrajbansi212@gmail.com>
2024-05-05 10:26:37 +0545

    Change
    Change in about page description

--- client/src/components/AboutPage/AboutPage.jsx
+++ client/src/components/AboutPage/AboutPage.jsx
@@ -9,8 +9,8 @@ const AboutPage = () => {
       <h1>About Us</h1>
       <div className="about-content">
         <div className="about-description">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero in dui convallis, vitae bibendum libero feugiat. Duis posuere eros ut mi ullamcorper malesuada.</p>
-          <p>Fusce non libero vel ligula iaculis tincidunt. Nulla id ex at magna elementum fringilla. Quisque dignissim arcu auctor feugiat feugiat.</p>
+          <p>At our store, we curate a meticulously crafted collection of headphones that cater to every audiophile's needs, from the casual listener to the discerning connoisseur. Whether you're seeking the immersive soundstage of over-ear headphones, the portability of sleek in-ear buds, or the freedom of wireless connectivity, we have the perfect pair waiting for you.</p>
+          <p>Indulge your senses with our selection of top-tier brands renowned for their innovation and craftsmanship. From industry giants pushing the boundaries of audio technology to boutique manufacturers with a passion for sonic excellence, we bring you the best of the best.</p>
         </div>
         <div className="about-images">
           <img src={teamImage1} alt="Team 1" />
